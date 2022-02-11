71b6127f2ff7e2c1c6108229f8b1e2ce4916db23
Flame_ <99051294+ThatsFlame@users.noreply.github.com>
2022-02-11 19:02:06 +0100

    Add files via upload

---
+++ Discord spammer/spammer.py
@@ -0,0 +1,16 @@
+import requests
+
+chan_id = "insert channel id"
+
+payload = {
+    "content" : "sesso oscuro xd"
+}
+
+header = {
+    'authorization' : "token"
+}
+while True:
+    r = requests.post(f"https://discord.com/api/v9/channels/{chan_id}/messages",
+                   data=payload, headers=header)
+
+                  
